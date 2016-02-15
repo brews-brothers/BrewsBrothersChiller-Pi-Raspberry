44a9693707703811c5d46e5bbd460cd1db615be6
Noah Roston <rostonn@gmail.com>
2016-02-15 13:41:46 -0700

    Added start cycle function

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/startcycle', startcycle);
+app.use('/startcycle', startcycle.router);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- routes/startcycle.js
+++ routes/startcycle.js
@@ -2,18 +2,78 @@ var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcrypt');
 require('dotenv').load();
+var mongodb = require('mongodb');
 
+var startTime;
 
-
+var url = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/pidatabase';
 /* GET home page. */
 router.post('/', function(req, res, next) {
   var password = req.body.password;
   if(bcrypt.compareSync(process.env.SERVER_SECRET,password)){
-    res.send('valid password');
+    if(req.body.schedule){
+      console.log(req.body.schedule);
+      mongodb.MongoClient.connect(url,function(err,db){
+        if(err){
+          throw err;
+        }
+        var schedule = db.collection('schedule');
+        schedule.remove();
+        schedule.insert({schedule:req.body.schedule},function(err,data){
+          if(err){
+            throw err;
+          }
+          startTime = Date.now();
+          //Open socket to backend server
+
+          monitorCycle();
+          res.send('started cycle')
+        })
+      })
+
+    }
+    else{
+      res.send('error no schedule')
+    }
   }
   else{
     res.send('Not a valid password');
   }
 });
 
-module.exports = router;
+function monitorCycle(){
+  var currentTime = Date.now();
+  var timeElapsed = currentTime - startTime;
+  var setTemp;
+
+  mongodb.MongoClient.connect(url,function(err,db){
+    if(err){
+      //socket emit
+    }
+    var schedule = db.collection('schedule');
+    schedule.find().toArray(function(err,results){
+      var scheduleArray = JSON.parse(results[0].schedule);
+      console.log(scheduleArray);
+      for(var i = 0; i < scheduleArray.length; i++){
+        if(scheduleArray[i][0] <= timeElapsed){
+          if(!setTemp){
+            setTemp = scheduleArray[i][1];
+            var setTime = scheduleArray[i][0];
+          }
+          else{
+            if(scheduleArray[i][0] > setTime){
+              setTemp = scheduleArray[i][1];
+            }
+          }
+        }
+      }
+      console.log(setTemp);
+    })
+  })
+
+}
+
+module.exports = {
+  router:router,
+  startTime:startTime
+}
